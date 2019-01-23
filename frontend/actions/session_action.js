7b9a235bcac19e7f2e28214f2409f6d2458ef932
William Falcone <wjf0003@gmail.com>
2019-01-23 09:56:15 -0500

    session actions built

---
+++ frontend/actions/session_action.js
@@ -0,0 +1,46 @@
+import * as sessionAPIUtil from './sessionAPIUtil';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = (currentUser) =>{
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser: currentUser 
+  }
+}
+
+export const logoutCurrentUser = () => {
+  return{
+    type: LOGOUT_CURRENT_USER
+  }
+}
+
+export const receiveErrors = (errors) => {
+  return{
+    type: RECEIVE_ERRORS,
+    errors 
+  }
+}
+
+export const login = (user) => dispatch => {
+  return sessionAPIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user))), err => (
+      dispatch(receiveErrors(err.responseJSON))
+    )
+}
+
+export const logout = () => dispatch => {
+  return sessionAPIUtil.logout()
+    .then((user) => dispatch(logoutCurrentUser()))
+}
+
+export const signup = (user) => dispatch => {
+  return sessionAPIUtil.signup(user)
+    .then( user => dispatch(receiveCurrentUser(user))), err => (
+      dispatch(receiveErrors(err.responseJSON))
+    )
+}
+
+
