a8ef17c4de63ff1f4890393d99dab97874db9ae7
William Falcone <wjf0003@gmail.com>
2019-01-23 10:42:02 -0500

    added actions/reducers for users sessions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as sessionAPIUtil from './sessionAPIUtil';
+import * as sessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
--- frontend/open_basil.jsx
+++ frontend/open_basil.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, signup, logout} from './util/session_api_util';
+import configureStore from '../frontend/store/store'
 
 document.addEventListener("DOMContentLoaded", ()=>{
-
+////tests
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+/// tests 
+
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Open Basil!</h1>, root)
