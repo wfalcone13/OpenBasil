65676efe65683c009ae52b206a1a93f9cc89827f
William Falcone <wjf0003@gmail.com>
2019-01-23 14:10:48 -0500

    added, trying to push to hq

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module OpenBasil
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
