0cd92bf3a9e030bf4183e4f27610b199b7385fec
William Falcone <wjf0003@gmail.com>
2019-01-30 10:03:43 -0500

    trying to push to hq

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -4,7 +4,6 @@ import {login} from '../../actions/session_actions';
 import {openModal, closeModal} from '../../actions/modal_actions';
 import SessionForm from './session_form';
 
-
 const mapStateToProp = ({errors}) => {
   return {
     errors: errors.session,
