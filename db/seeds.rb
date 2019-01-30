5bb0a0c4e556dec5341c640fd88a07ac65948e95
William Falcone <wjf0003@gmail.com>
2019-01-30 10:44:43 -0500

    test seeding images

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,23 +17,25 @@ rest_3 = Restaurant.new({name: 'Naked Dog', category: 'Northern Italian' , long:
 rest_4 = Restaurant.new({name: 'Brano', category: 'New Italian', long: 40.710734, lad: -73.967502, location: '26 Broadway, Brooklyn, NY 11249' })
 rest_5 = Restaurant.new({name: "Joe's Pizza", category: 'Pizza', long: 40.73363, lad: -73.98744, location: "150 E 14th St, New York, NY 10003"})
 
-
-file1 = File.open('/Users/william_falcone/Desktop/rest_pics/lilia.jpeg')
-rest_1.photo.attach(io: file1, filename: 'lilia.jpeg')
-rest_1.save!
-
-file2 = File.open('/Users/william_falcone/Desktop/rest_pics/best_pizza.jpeg')
-rest_2.photo.attach(io: file2, filename: 'best_pizza.jpeg')
-rest_2.save!
-
-file3 = File.open('/Users/william_falcone/Desktop/rest_pics/naked_dog.jpeg')
-rest_3.photo.attach(io: file3, filename: 'naked_dog.jpeg')
-rest_3.save!
-
-file4 = File.open('/Users/william_falcone/Desktop/rest_pics/barano.jpeg')
-rest_4.photo.attach(io: file4, filename: 'barano.jpeg')
-rest_4.save!
-
-file5 = File.open('/Users/william_falcone/Desktop/rest_pics/joessm.jpg')
-rest_5.photo.attach(io: file5, filename: 'joessm.jpg')
+file5 = File.open('https://s3.amazonaws.com/open-basil-dev/2d6KuQsUFXWS9C6EstXfHjSf')
+rest_5.photo.attach(io: file5, filename: '2d6KuQsUFXWS9C6EstXfHjSf')
 rest_5.save!
+
+# file1 = File.open('/Users/william_falcone/Desktop/rest_pics/lilia.jpeg')
+# rest_1.photo.attach(io: file1, filename: 'lilia.jpeg')
+# rest_1.save!
+
+# file2 = File.open('/Users/william_falcone/Desktop/rest_pics/best_pizza.jpeg')
+# rest_2.photo.attach(io: file2, filename: 'best_pizza.jpeg')
+# rest_2.save!
+
+# file3 = File.open('/Users/william_falcone/Desktop/rest_pics/naked_dog.jpeg')
+# rest_3.photo.attach(io: file3, filename: 'naked_dog.jpeg')
+# rest_3.save!
+
+# file4 = File.open('/Users/william_falcone/Desktop/rest_pics/barano.jpeg')
+# rest_4.photo.attach(io: file4, filename: 'barano.jpeg')
+# rest_4.save!
+
+
+puts 'FINISHED SEEDING'
