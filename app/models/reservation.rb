6cf95c3d39a817ad3f1afa2f42e381163567b84c
William Falcone <wjf0003@gmail.com>
2019-01-30 10:29:32 -0500

    Add presence:true in reserve model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord 
-  validates :user_id, :restaurant_id, :reservation_time, :reservation_date, :seating_number
+  validates :user_id, :restaurant_id, :reservation_time, :reservation_date, :seating_number, presence: true
 
   belongs_to :user 
   belongs_to :restaurant 
